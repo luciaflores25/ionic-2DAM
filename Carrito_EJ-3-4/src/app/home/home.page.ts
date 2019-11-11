ece2fc82fbadeb44e3d4f65c492642f0169c220f
Lucía Flores Padilla <lucia.flores.padilla@gmail.com>
2019-11-11 15:35:43 +0100

    Ejercicio carrito relación de ejercicios 2

---
+++ Carrito_EJ-3-4/src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Item } from '../interfaces/item';
+import { ItemService } from '../services/item.service';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  private items: Item[];
+  private total: number;
+
+  constructor(private service: ItemService) {}
+  
+  ngOnInit() {
+    this.total = 0;
+    this.items = this.service.getItems();
+  }
+
+  getTotal(): number {
+    let total = 0;
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.items.length; i++) {
+      total = total + this.items[i].subtotal;
+    }
+    return total;
+  }
+}
